682b45ee819f6e2664578f6ba3b0791226a84785
Indra Han <indrah95@gmail.com>
2020-06-27 20:09:19 +0200

    implemented timer and end goal

---
+++ Assets/End.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+private void OnTriggerEnter2D(Collider2D collision){
+  if (collision.transform.tag == "Player"){
+    SceneManager.LoadScene(sceneName);
+  }
+}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+  float currentTime = 0.0f;
+  float startingTime = 0.0f;
+
+  [SerializeField] Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += 1 * (Time.deltaTime);
+        countdownText.text = currentTime.ToString("0.0");
+    }
+}
